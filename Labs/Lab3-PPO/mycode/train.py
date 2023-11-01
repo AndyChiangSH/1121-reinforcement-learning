47e37c3c42f3a744e4a97cc7f915975fbabd6224
AndyChiangSH <andy10801@gmail.com>
2023-11-01 19:07:04 +0800

    Train PPO-7

--- Labs/Lab3-PPO/mycode/train.py
+++ Labs/Lab3-PPO/mycode/train.py
@@ -65,6 +65,27 @@ if __name__ == '__main__':
     # }
     
     # My
+    # config = {
+    #     "gpu": True,
+    #     "training_steps": 1e8,
+    #     "update_sample_count": 10000,
+    #     "discount_factor_gamma": 0.99,
+    #     "discount_factor_lambda": 0.95,
+    #     "clip_epsilon": 0.2,
+    #     "max_gradient_norm": 0.5,
+    #     "batch_size": 128,
+    #     "logdir": 'log/PPO-6/',
+    #     "update_ppo_epoch": 3,
+    #     "learning_rate": 1e-5,
+    #     "value_coefficient": 0.5,
+    #     "entropy_coefficient": 0.01,
+    #     "horizon": 128,
+    #     "env_id": 'ALE/Enduro-v5',
+    #     "eval_interval": 100,
+    #     "eval_episode": 3,
+    # }
+    
+    # My
     config = {
         "gpu": True,
         "training_steps": 1e8,
@@ -74,9 +95,9 @@ if __name__ == '__main__':
         "clip_epsilon": 0.2,
         "max_gradient_norm": 0.5,
         "batch_size": 128,
-        "logdir": 'log/PPO-6/',
+        "logdir": 'log/PPO-7/',
         "update_ppo_epoch": 3,
-        "learning_rate": 1e-5,
+        "learning_rate": 1e-6,
         "value_coefficient": 0.5,
         "entropy_coefficient": 0.01,
         "horizon": 128,
@@ -86,5 +107,5 @@ if __name__ == '__main__':
     }
 
     agent = AtariPPOAgent(config)
-    agent.load("./log/PPO-3/model_77191616_1657.pth")
+    agent.load("./log/PPO-6/model_29698988_2292.pth")
     agent.train()
