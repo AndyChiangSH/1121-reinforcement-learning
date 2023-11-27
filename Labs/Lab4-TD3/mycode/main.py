e588bea312dcecd5137dccdfba1040e0f815bb7f
AndyChiangSH <andy10801@gmail.com>
2023-11-27 11:57:48 +0800

    Lab4: TD3-4

--- Labs/Lab4-TD3/mycode/main.py
+++ Labs/Lab4-TD3/mycode/main.py
@@ -38,6 +38,24 @@ if __name__ == '__main__':
     # 	"render": False,
     # }
  
+    # config = {
+    #     "gpu": True,
+    #     "training_steps": 1e8,
+    #     "gamma": 0.99,
+    #     "tau": 0.005,
+    #     "batch_size": 32,
+    #     "warmup_steps": 1000,
+    #     "total_episode": 100000,
+    #     "lra": 4.5e-5,
+    #     "lrc": 4.5e-5,
+    #     "replay_buffer_capacity": 5000,
+    #     "logdir": 'log/TD3-3',
+    #     "update_freq": 4,
+    #     "eval_interval": 100,
+    #     "eval_episode": 10,
+    #     "render": False,
+    # }
+    
     config = {
         "gpu": True,
         "training_steps": 1e8,
@@ -49,13 +67,13 @@ if __name__ == '__main__':
         "lra": 4.5e-5,
         "lrc": 4.5e-5,
         "replay_buffer_capacity": 5000,
-        "logdir": 'log/TD3-3',
+        "logdir": 'log/TD3-4',
         "update_freq": 4,
         "eval_interval": 100,
         "eval_episode": 10,
         "render": False,
     }
-     
+
     agent = CarRacingTD3Agent(config)
     agent.train()
 
