88d7f3ee3eac2935d8674b789c2164401233c4a0
AndyChiangSH <andy10801@gmail.com>
2023-12-02 16:59:07 +0800

    Train TD3-12

--- Labs/Lab4-TD3/mycode/environment_wrapper/CarRacingEnv.py
+++ Labs/Lab4-TD3/mycode/environment_wrapper/CarRacingEnv.py
@@ -11,113 +11,120 @@ import torch
 import torch.nn as nn
 from torch.utils.tensorboard import SummaryWriter
 
+import math
+
+
 class CarRacingEnvironment:
-	def __init__(self, N_frame=4, test=False, render=False):
-		self.test = test
-		if render:
-			self.env = gym.make('CarRacing-v2', render_mode="human")
-			# self.env = gym.make('CarRacing-v2')
-		else:
-			self.env = gym.make('CarRacing-v2')
-		self.action_space = self.env.action_space
-		self.observation_space = self.env.observation_space
-		self.ep_len = 0
-		self.frames = deque(maxlen=N_frame)
-	
-	def check_car_position(self, obs):
-		# cut the image to get the part where the car is
-		part_image = obs[60:84, 40:60, :]
+    def __init__(self, N_frame=4, test=False, render=False):
+        self.test = test
+        if render:
+            self.env = gym.make('CarRacing-v2', render_mode="human")
+            # self.env = gym.make('CarRacing-v2')
+        else:
+            self.env = gym.make('CarRacing-v2')
+        self.action_space = self.env.action_space
+        self.observation_space = self.env.observation_space
+        self.ep_len = 0
+        self.frames = deque(maxlen=N_frame)
+    
+    def check_car_position(self, obs):
+        # cut the image to get the part where the car is
+        part_image = obs[60:84, 40:60, :]
 
-		road_color_lower = np.array([90, 90, 90], dtype=np.uint8)
-		road_color_upper = np.array([120, 120, 120], dtype=np.uint8)
-		grass_color_lower = np.array([90, 180, 90], dtype=np.uint8)
-		grass_color_upper = np.array([120, 255, 120], dtype=np.uint8)
-		road_mask = cv2.inRange(part_image, road_color_lower, road_color_upper)
-		grass_mask = cv2.inRange(part_image, grass_color_lower, grass_color_upper)
-		# count the number of pixels in the road and grass
-		road_pixel_count = cv2.countNonZero(road_mask)
-		grass_pixel_count = cv2.countNonZero(grass_mask)
+        road_color_lower = np.array([90, 90, 90], dtype=np.uint8)
+        road_color_upper = np.array([120, 120, 120], dtype=np.uint8)
+        grass_color_lower = np.array([90, 180, 90], dtype=np.uint8)
+        grass_color_upper = np.array([120, 255, 120], dtype=np.uint8)
+        road_mask = cv2.inRange(part_image, road_color_lower, road_color_upper)
+        grass_mask = cv2.inRange(part_image, grass_color_lower, grass_color_upper)
+        # count the number of pixels in the road and grass
+        road_pixel_count = cv2.countNonZero(road_mask)
+        grass_pixel_count = cv2.countNonZero(grass_mask)
 
-		# save image for debugging
-		# filename = "images/image" + str(self.ep_len) + ".jpg"
-		# cv2.imwrite(filename, part_image)
+        # save image for debugging
+        # filename = "images/image" + str(self.ep_len) + ".jpg"
+        # cv2.imwrite(filename, part_image)
 
-		return road_pixel_count, grass_pixel_count
+        return road_pixel_count, grass_pixel_count
 
-	def step(self, action):
-		obs, reward, terminates, truncates, info = self.env.step(action)
-		original_reward = reward
-		original_terminates = terminates
-		self.ep_len += 1
-		road_pixel_count, grass_pixel_count = self.check_car_position(obs)
-		info["road_pixel_count"] = road_pixel_count
-		info["grass_pixel_count"] = grass_pixel_count
+    def step(self, action):
+        obs, reward, terminates, truncates, info = self.env.step(action)
+        original_reward = reward
+        original_terminates = terminates
+        self.ep_len += 1
+        road_pixel_count, grass_pixel_count = self.check_car_position(obs)
+        info["road_pixel_count"] = road_pixel_count
+        info["grass_pixel_count"] = grass_pixel_count
 
-		# my reward shaping strategy, you can try your own
-		if road_pixel_count < 10:
-			terminates = True
-			reward = -100
+        # my reward shaping strategy, you can try your own
+        if road_pixel_count < 10:
+            terminates = True
+            reward = -100
 
-		# my reward function
-		reward += 0.1 * road_pixel_count
-		# reward -= 0.1 * grass_pixel_count
+        # my reward function
+        # reward -= 0.1 * grass_pixel_count
+        # reward += 0.1 * road_pixel_count
+        try:
+            reward -= grass_pixel_count / road_pixel_count
+        except:
+            reward -= grass_pixel_count
   
-		# convert to grayscale
-		obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY) # 96x96
+        # convert to grayscale
+        obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY) # 96x96
 
-		# save image for debugging
-		# filename = "images/image" + str(self.ep_len) + ".jpg"
-		# cv2.imwrite(filename, obs)
+        # save image for debugging
+        # filename = "images/image" + str(self.ep_len) + ".jpg"
+        # cv2.imwrite(filename, obs)
 
-		# frame stacking
-		self.frames.append(obs)
-		obs = np.stack(self.frames, axis=0)
+        # frame stacking
+        self.frames.append(obs)
+        obs = np.stack(self.frames, axis=0)
 
-		if self.test:
-			# enable this line to recover the original reward
-			reward = original_reward
-			# enable this line to recover the original terminates signal, disable this to accerlate evaluation
-			# terminates = original_terminates
+        if self.test:
+            # enable this line to recover the original reward
+            reward = original_reward
+            # enable this line to recover the original terminates signal, disable this to accerlate evaluation
+            # terminates = original_terminates
 
-		return obs, reward, terminates, truncates, info
-	
-	def reset(self):
-		obs, info = self.env.reset()
-		self.ep_len = 0
-		obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY) # 96x96
+        return obs, reward, terminates, truncates, info
+    
+    def reset(self):
+        obs, info = self.env.reset()
+        self.ep_len = 0
+        obs = cv2.cvtColor(obs, cv2.COLOR_BGR2GRAY) # 96x96
 
-		# frame stacking
-		for _ in range(self.frames.maxlen):
-			self.frames.append(obs)
-		obs = np.stack(self.frames, axis=0)
+        # frame stacking
+        for _ in range(self.frames.maxlen):
+            self.frames.append(obs)
+        obs = np.stack(self.frames, axis=0)
 
-		return obs, info
-	
-	def render(self):
-		self.env.render()
-	
-	def close(self):
-		self.env.close()
+        return obs, info
+    
+    def render(self):
+        self.env.render()
+    
+    def close(self):
+        self.env.close()
 
 if __name__ == '__main__':
-	env = CarRacingEnvironment(test=True)
-	obs, info = env.reset()
-	done = False
-	total_reward = 0
-	total_length = 0
-	t = 0
-	while not done:
-		t += 1
-		action = env.action_space.sample()
-		action[2] = 0.0
-		obs, reward, terminates, truncates, info = env.step(action)
-		print(f'{t}: road_pixel_count: {info["road_pixel_count"]}, grass_pixel_count: {info["grass_pixel_count"]}, reward: {reward}')
-		total_reward += reward
-		total_length += 1
-		# env.render()
-		if terminates or truncates:
-			done = True
+    env = CarRacingEnvironment(test=True)
+    obs, info = env.reset()
+    done = False
+    total_reward = 0
+    total_length = 0
+    t = 0
+    while not done:
+        t += 1
+        action = env.action_space.sample()
+        action[2] = 0.0
+        obs, reward, terminates, truncates, info = env.step(action)
+        print(f'{t}: road_pixel_count: {info["road_pixel_count"]}, grass_pixel_count: {info["grass_pixel_count"]}, reward: {reward}')
+        total_reward += reward
+        total_length += 1
+        # env.render()
+        if terminates or truncates:
+            done = True
 
-	print("Total reward: ", total_reward)
-	print("Total length: ", total_length)
-	env.close()
+    print("Total reward: ", total_reward)
+    print("Total length: ", total_length)
+    env.close()
--- Labs/Lab4-TD3/mycode/main.py
+++ Labs/Lab4-TD3/mycode/main.py
@@ -182,6 +182,24 @@ if __name__ == '__main__':
     #     "render": False,
     # }
 
+    # config = {
+    #     "gpu": True,
+    #     "training_steps": 1e8,
+    #     "gamma": 0.99,
+    #     "tau": 0.005,
+    #     "batch_size": 32,
+    #     "warmup_steps": 1000,
+    #     "total_episode": 100000,
+    #     "lra": 4.5e-5,
+    #     "lrc": 4.5e-5,
+    #     "replay_buffer_capacity": 5000,
+    #     "logdir": 'log/TD3-11',
+    #     "update_freq": 2,
+    #     "eval_interval": 100,
+    #     "eval_episode": 10,
+    #     "render": False,
+    # }
+
     config = {
         "gpu": True,
         "training_steps": 1e8,
@@ -193,12 +211,13 @@ if __name__ == '__main__':
         "lra": 4.5e-5,
         "lrc": 4.5e-5,
         "replay_buffer_capacity": 5000,
-        "logdir": 'log/TD3-11',
+        "logdir": 'log/TD3-12',
         "update_freq": 2,
         "eval_interval": 100,
         "eval_episode": 10,
         "render": False,
     }
+
 
     agent = CarRacingTD3Agent(config)
     agent.train()
