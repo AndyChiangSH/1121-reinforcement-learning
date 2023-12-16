702ca15561cbf8294050fd4a06602a1ff16c71fb
AndyChiangSH <andy10801@gmail.com>
2023-12-16 11:13:16 +0800

    Final project: train TD3-circle-18

--- Final Project/final_project_env/TD3/train.py
+++ Final Project/final_project_env/TD3/train.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
 		"update_freq": 2, #B3
 		"eval_interval": 100,
 		"eval_episode": 10,
-		"logdir": 'TD3/log/TD3-circle-17',
+		"logdir": 'TD3/log/TD3-circle-18',
 		"scenario": "circle_cw_competition_collisionStop"
 	}
 
